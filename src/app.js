cce20181e5ffc444473774fe2125c1ee0729f908
khanhhn <chienbinhbattu9xpro@gmail.com>
2024-01-15 23:04:33 +0700

    [Develop] Update code

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
+require('dotenv').config()
 const compression = require('compression')
 const express = require('express')
 const { default: helmet } = require('helmet')
 const morgan = require('morgan')
 const app = express()
-
 
 // init middlewares
 app.use(morgan('dev'))
@@ -13,7 +13,7 @@ app.use(compression())
 // init db
 require('./dbs/init.mongodb')
 const { checkOverload } = require('./helpers/check.connect')
-checkOverload()
+// checkOverload()
 // init routes
 app.get('/', (req, res, next) => {
     const strCompress = 'Hello world!'
